ca2a3ebe263647747dd7e7da7a8619516ec89d8e
mmbapin <bapinmm@gmail.com>
2025-03-24 00:55:34 +0600

    106 no done

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import NavLink from './nav-link';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from 'next/image';
@@ -18,10 +19,10 @@ const MainHeader = () => {
 				<nav className={classes.nav}>
 					<ul>
 						<li>
-							<Link href='/meals'>Browse Meals</Link>
+							<NavLink href='/meals'>Browse Meals</NavLink>
 						</li>
 						<li>
-							<Link href='/community'>Foodies Community</Link>
+							<NavLink href='/community'>Foodies Community</NavLink>
 						</li>
 					</ul>
 				</nav>
