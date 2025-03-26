482ef1826c5683376923f60fe3b87d01dd8a2dbe
mmbapin <bapinmm@gmail.com>
2025-03-27 00:33:35 +0600

    109 no done

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import classes from './page.module.css';
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-const MealsPage = () => {
-	return <div>MealsPage</div>;
+const MealsPage = async () => {
+	const meals = await getMeals()
+	return (
+		<>
+			<header className={classes.header}>
+				<h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
+				<p>Choose your favourite recipe and cook it yourself. It is easy and fun!</p>
+				<p className={classes.cta}>
+					<Link href="/meals/share">Share Your Favourite Recipe</Link>
+				</p>
+			</header>
+			<main className={classes.main}>
+				<MealsGrid meals={meals}/>
+			</main>
+		</>
+	)
 };
 
 export default MealsPage;
