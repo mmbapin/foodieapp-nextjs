1e10db5e1d2805e9f5575cbb7d8179c1aa49e679
mmbapin <bapinmm@hmail.com>
2025-04-10 18:12:51 +0600

    118 done

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,16 +1,19 @@
 'use client'
 import React from 'react'
 import classes from './image-picker.module.css'
+import { useRef } from'react';
 
 const ImagePicker = ({label, name}) => {
+  const imageInput = useRef();
   const handlePickClick = () => {
-    console.log("Hello")
+    console.log("Hello");
+    imageInput.current.click();
   }
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
-        <input className={classes.input} type='file' id={name} name={name} accept='image/png, image/jpeg' />
+        <input className={classes.input} type='file' id={name} name={name} accept='image/png, image/jpeg' ref={imageInput}/>
         <button className={classes.button} type='button' onClick={handlePickClick}>Choose Image</button>
       </div>
     </div>
