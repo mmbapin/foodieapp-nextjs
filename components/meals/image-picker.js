cb06b4de0e61d14f85f4abdc96727cd3c86ef119
mmbapin <bapinmm@gmail.com>
2025-04-10 21:48:29 +0600

    121 Done

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,23 +1,57 @@
-'use client'
-import React from 'react'
-import classes from './image-picker.module.css'
-import { useRef } from'react';
+'use client';
+import React from 'react';
+import classes from './image-picker.module.css';
+import { useRef, useState } from 'react';
+import Image from 'next/image';
 
-const ImagePicker = ({label, name}) => {
-  const imageInput = useRef();
-  const handlePickClick = () => {
-    console.log("Hello");
-    imageInput.current.click();
-  }
-  return (
-    <div className={classes.picker}>
-      <label htmlFor={name}>{label}</label>
-      <div className={classes.controls}>
-        <input className={classes.input} type='file' id={name} name={name} accept='image/png, image/jpeg' ref={imageInput}/>
-        <button className={classes.button} type='button' onClick={handlePickClick}>Choose Image</button>
-      </div>
-    </div>
-  )
-}
+const ImagePicker = ({ label, name }) => {
+	const [pickedImage, setPickedImage] = useState(null);
+	const imageInput = useRef();
+	const handlePickClick = () => {
+		console.log('Hello');
+		imageInput.current.click();
+	};
 
-export default ImagePicker
+	const handleImageChange = (event) => {
+		const file = event.target.files[0];
+
+		if (!file) {
+			setPickedImage(null);
+			return;
+		}
+
+		const fileReader = new FileReader();
+
+		fileReader.onload = () => {
+			setPickedImage(fileReader.result);
+			// URL.revokeObjectURL(fileReader.result);
+		};
+
+		fileReader.readAsDataURL(file);
+	};
+	return (
+		<div className={classes.picker}>
+			<label htmlFor={name}>{label}</label>
+			<div className={classes.controls}>
+				<div className={classes.preview}>
+					{!pickedImage && <p>No image chosen yet!</p>}
+					{pickedImage && <Image src={pickedImage} alt='Preview' fill />}
+				</div>
+				<input
+					className={classes.input}
+					type='file'
+					id={name}
+					name={name}
+					accept='image/png, image/jpeg'
+					onChange={handleImageChange}
+					ref={imageInput}
+				/>
+				<button className={classes.button} type='button' onClick={handlePickClick}>
+					Choose Image
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default ImagePicker;
